e4081ec70968a47a35dcdf4c89778d9d6029ecb9
Salma Gamal <Salma.GamalCS21@fcai.usc.edu.eg>
2025-06-14 23:15:54 +0300

    Final

--- src/components/Admin/Login.jsx
+++ src/components/Admin/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import "./Login.css";
+import "./login.css";
 
 const Login = ({ onLogin }) => {
   const [username, setUsername] = useState("");
